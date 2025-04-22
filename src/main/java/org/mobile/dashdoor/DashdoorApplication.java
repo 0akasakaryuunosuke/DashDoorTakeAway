09663ac73fe3cf380d26806dc75fb5ea2282f9e9
0akasakaryuunosuke <methylbird@gmail.com>
2025-04-22 19:14:36 +0800

    commit 测试

---
+++ src/main/java/org/mobile/dashdoor/DashdoorApplication.java
@@ -0,0 +1,13 @@
+package org.mobile.dashdoor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashdoorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DashdoorApplication.class, args);
+    }
+
+}
